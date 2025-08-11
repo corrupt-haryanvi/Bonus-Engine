1c06d190c42206ce25c2a1b75a82135ce31e5d83
Sombir Chahal <smbrchahal@gmail.com>
2025-08-11 16:04:25 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,36 @@
-self.addEventListener('install', e => {
+const STATIC_CACHE = "bonus-cache-v3"; // bump this
+
+self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open('bonus-cache-v1').then(cache => cache.addAll([
-      './',
-      './index.html',
-      './app.js',
-      './tiers.json',
-      './manifest.webmanifest',
-      './icons/icon-192.png',
-      './icons/icon-512.png',
-      './icons/maskable-512.png'
-    ]))
+    caches.open(STATIC_CACHE).then((cache) =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./app.js",
+        "./manifest.webmanifest",
+        "./icons/icon-192.png",
+        "./icons/icon-512.png",
+        "./icons/maskable-512.png"
+        // DO NOT add tiers.json here
+      ])
+    )
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
-  );
+// Network-first for tiers.json
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+  if (url.pathname.endsWith("/tiers.json")) {
+    e.respondWith(
+      fetch(e.request)
+        .then((resp) => {
+          const clone = resp.clone();
+          caches.open(STATIC_CACHE).then((c) => c.put(e.request, clone));
+          return resp;
+        })
+        .catch(() => caches.match(e.request))
+    );
+    return;
+  }
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
